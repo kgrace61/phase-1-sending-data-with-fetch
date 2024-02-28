504df6152ffe076120671cba29253772d0b6060f
Kristen <kgrace61@gmail.com>
2024-02-27 23:43:26 -0600

    did my lil labby lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData (name, email) {
+    return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({name, email})
+})
+    .then(res => res.json())
+    .then(data => {
+        let newId = data.id;
+        document.body.append(newId);
+        console.log(submitData)
+    })
+    .catch(error => {
+        const errorMessage = document.createElement('div');
+        errorMessage.textContent = error.message;
+        document.body.append(errorMessage);
+        console.error('Error', error);
+    });
+
+
+    }
